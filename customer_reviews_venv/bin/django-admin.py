991bc9dff43f2b045eb51ee9bccb41c70a79fbfe
Vedant Nanda <vedant15114@iiitd.ac.in>
2018-04-29 23:37:00 +0530

    webapp started

---
+++ customer_reviews_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vedant/Desktop/IR/project/Actionable-Intelligence/customer_reviews_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
