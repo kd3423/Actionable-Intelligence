19b034042370d2b1c7cac27511f3370aada0485c
Vedant Nanda <vedant15114@iiitd.ac.in>
2018-04-30 08:25:52 +0530

    wip

--- customer_reviews_webapp/customer_reviews_webapp/settings.py
+++ customer_reviews_webapp/customer_reviews_webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a_e3#u(06n_i*v0xn28a_n+ym9tssnony5w*0v%^dwh6fzlj6_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'customer_reviews_webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"templates"),]
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR
--- customer_reviews_webapp/src/views.py
+++ customer_reviews_webapp/src/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from scrape import scrape
 
 # Create your views here.
+def home(request):
+	if request.method == 'GET':
+		context = {
+			'heading':'Enter the link of a product whose reviews you want to analyze'
+		}
+	if request.method == 'POST':
+		scrape(request.POST.get('url_text'))
+	return render(request,'index.html',context)
