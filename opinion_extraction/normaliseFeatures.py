56999403a64af06dd8688b5a4e13f2d114761347
aklife97 <aklife97@gmail.com>
2018-04-30 09:25:59 +0530

    added fuzzy stuff

---
+++ opinion_extraction/normaliseFeatures.py
@@ -0,0 +1,27 @@
+f = open("fuzzy_dict.txt")
+fuzzy = eval(f.read())
+f.close()
+
+f = open("opinion_words.txt")
+opinions = eval(f.read())
+f.close()
+
+new_opinion = {}
+
+for op in opinions:
+	for fuz in fuzzy:
+		if op in fuzzy[fuz]:
+			# print opinions[op]
+			if fuz in new_opinion:
+				new_opinion[fuz] += opinions[op]
+			else:
+				new_opinion[fuz] = opinions[op]
+			# print fuz, op
+			break
+			# print new_opinions[fuz]
+	else:
+		new_opinion[op] = opinions[op]
+
+f = open("opinion_words.txt","w")
+f.write(str(new_opinion))
+f.close()
